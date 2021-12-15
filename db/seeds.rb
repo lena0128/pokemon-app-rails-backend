9feab0cb787e901d2ff0504e06980e79b6b0ed1d
Lena Hou <lenahou2013@gmail.com>
2021-12-15 11:09:16 -0500

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+response = HTTParty.get('https://pokeapi.co/api/v2/pokemon?limit=100000')
+pokemonArray = response['results']
+total = pokemonArray.count
+count = 0
+
+pokemonArray.each do |pokemonHash|
+    pokemon = HTTParty.get(pokemonHash['url'])
+    pokemonHash = {
+        name: pokemon['name'],
+        poke_type: pokemon['types'][0]['type']['name'],
+        weight: pokemon['weight'],
+        front_image: pokemon['sprites']['front_default'],
+        back_image: pokemon['sprites']['back_default'],
+    }
+    Pokemon.find_or_create_by(pokemonHash)
+    system('clear')
+    puts "Seeding Pokemon Data"
+    puts "-----------------"
+    puts "#{count/total.to_f*100}%"
+    count += 1
+end
+
+puts "-----------------"
+puts "Pokemon Data: 100%"
+puts "Data Sucessfully Seeded!"
+puts "-----------------"
