da16caf02382062cc9d294d219b6c67351c3c0bf
Lena Hou <lenahou2013@gmail.com>
2021-12-15 11:27:41 -0500

    set up pokemon controller

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,2 +1,44 @@
 class PokemonsController < ApplicationController
+    before_action :set_pokemon, only: [:show, :update, :destroy] 
+
+    def index
+        pokemons = Pokemon.all
+        render json: pokemons
+    end
+
+    def show
+        render json: pokemon
+    end
+
+    def create
+        pokemon = Pokemon.new(pokemon_params)
+
+        if pokemon.save
+            render json: pokemon, status: :created, location: pokemon
+        else
+            render json: pokemon.errors, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        if pokemon.update(pokemon_params)
+            render json: pokemon
+        else
+            render json: pokemon.errors, status: :unprocessable_entity
+    end
+
+    def destroy
+        pokemon.destroy
+        render json: {message: "Successfully deleted!"}
+    end
+
+    private
+    def pokemon_params
+        params.require(:pokemon).permit(:name, :poke_type, :weight, :front_image, :back_image)
+    end
+
+    def set_pokemon
+        pokemon = Pokemon.find_by(id: params[:id])
+    end
+
 end
